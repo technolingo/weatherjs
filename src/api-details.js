0968df661ff136ff26645ae4389788f9a68be30c
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2019-02-10 15:38:36 +0900

    search weather event handler

---
+++ src/api-details.js
@@ -0,0 +1,2 @@
+export const APIURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+export const APIKEY = '464d95b1676ba3d0e81525b2e18eeb84';
--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
 import * as ELEMENTS from 'elements.js';
+import * as APIDETAILS from 'api-details.js';
+import { WeatherAPIClient } from 'weather.js';
+
+ELEMENTS.SEARCH_BUTTON.addEventListener('click', getWeather);
+ELEMENTS.SEARCH_INPUT.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    getWeather();
+  }
+});
+
+function getWeather () {
+  const cityName = ELEMENTS.SEARCH_INPUT.value.trim();
+  if (cityName.length === 0) {
+    alert('Please enter a city name!');
+  }
+  // get weather data
+  const URL = APIDETAILS.APIURL + cityName + '&unit=metric&appid=' + APIDETAILS.APIKEY;
+  WeatherAPIClient.fetchData(URL)
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      alert(error);
+    });
+}
